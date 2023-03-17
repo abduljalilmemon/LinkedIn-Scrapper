ace61a6dd127de27e201cce65b296f842fbad787
Abdul Jalil <abduljaleelmemon3@gmail.com>
2023-03-17 23:54:13 +0500

    added scrapy and spider

---
+++ src/settings.py
@@ -0,0 +1,32 @@
+ # Scrapy settings for quotes_js_scraper project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'basic_scrapy_spider'
+
+SPIDER_MODULES = ['basic_scrapy_spider.spiders']
+NEWSPIDER_MODULE = 'basic_scrapy_spider.spiders'
+
+SCRAPEOPS_API_KEY = 'xyz'
+SCRAPEOPS_PROXY_ENABLED = True
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'quotes_js_scraper (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 1
+
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+   'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
